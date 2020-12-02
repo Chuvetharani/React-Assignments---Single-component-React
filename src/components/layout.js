00fa5c3ec107b292b1b610f8b881d03d379e4dd7
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-12-02 19:22:52 +0530

    changes1

---
+++ src/components/layout.js
@@ -0,0 +1,12 @@
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+const layoutComponent = () => {
+  return (
+    <div id="root">
+      <h1> Hi! I'm layout component! </h1>
+    </div>
+  );
+};
+
+export default layoutComponent;
